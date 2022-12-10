3b7a940e241b03b734b0c16e0c58dd402f084451
tormobr <tormod.brandshoi@gmail.com>
2022-12-11 00:40:18 +0100

    made solution more pretty for day 07

--- 07/solution.py
+++ 07/solution.py
@@ -12,87 +12,51 @@ from advent_lib import *
 
 
 class shell():
-    # Part 1 solution : 
-    def part_1(self):
-        directories = self.build_tree()
+    def __init__(self):
+        self.cwd = ""
+        self.commands = {
+            "cd": self.handle_cd,
+            "ls": self.handle_ls
+        }
+        self.build_tree()
 
-        sizes = [self.dfs(key, directories) for key in directories.keys()]
-        return sum(s for s in sizes if s <= 100000)
-
-    # Part 2 solution : 
-    def part_2(self):
-        directories = self.build_tree()
-
-        total_space = 70000000
-        required = 30000000
-
-        total_used = self.dfs("//", directories)
-        not_used = total_space - total_used
-        to_be_freed = required - not_used
-
-        sizes = [self.dfs(key, directories) for key in directories.keys()]
-        return min(s for s in sizes if s >= to_be_freed)
-
+    # -------------------------------------------------------- Parsing ----------------------------------------------------------
     def build_tree(self):
+        self.cwd = ""
         data = read_lines("input.txt", f=str)
 
-        directories = defaultdict(lambda: defaultdict(list))
-        current_dir = ""
-        index = 0
+        self.directories = defaultdict(lambda: defaultdict(list))
 
+        index = 0
         while index < len(data):
             command = data[index]
-
             output = self.get_output(data, index)
 
-            if self.check_command(command, "cd"):
-                # handle_cd(command, directories, output, current_dir)
-                args = self.get_args(command)
-                if args == "..":
-                    current_dir = "/".join(current_dir.split("/")[:-2]) + "/"
-                else:
-                    current_dir += args + "/"
-            elif self.check_command(command, "ls"):
-                # handle_ls(directories, output, current_dir)
-                for out in output:
-                    if self.is_dir(out):
-                        directories[current_dir]["dirs"].append(current_dir + out.split()[1] + "/")
-                    else:
-                        directories[current_dir]["files"].append(self.get_file(out))
+            self.commands[self.get_command_str(command)](command, output)
+            index += len(output) + 1 # index jump to next command
 
-            index += len(output) + 1
-
-        return directories
-
-    def handle_ls(self, directories, output):
+    def handle_ls(self, command, output):
         for out in output:
-            if self.is_dir(out):
-                directories[current_dir]["dirs"].append(current_dir + out.split()[1] + "/")
-            else:
-                directories[current_dir]["files"].append(self.get_file(out))
+            is_dir = self.is_dir(out)
 
-    def handle_cd(self, directories, output):
-        args = get_args(command)
-        if args == "..":
-            current_dir = "/".join(current_dir.split("/")[:-2]) + "/"
-        else:
-            current_dir += args + "/"
+            key = "dirs" if is_dir else "files"
+            to_add = self.move_dir_down(self.get_dir_info(out)) if is_dir else self.get_file_info(out)
+            self.directories[self.cwd][key].append(to_add)
 
+    def handle_cd(self, command, output):
+        args = self.get_args(command)
+        self.cwd = self.move_dir_up() if args[0] == ".." else self.move_dir_down(args[0])
 
+    def move_dir_up(self):
+        return "/".join(self.cwd.split("/")[:-2]) + "/"
 
+    def move_dir_down(self, new_dir):
+        new_current_dir = self.cwd + new_dir
+        new_current_dir += "/" if new_current_dir != "/" else ""
+        return new_current_dir
 
-    def dfs(self, current_dir, directories):
-        files = directories[current_dir]["files"]
-
-        total = 0
-        for size, filename in files:
-            total += size
-
-        for child in directories[current_dir]["dirs"]:
-            total += self.dfs(child, directories)
-
-        return total
-
+    def get_command_str(self, s):
+        return s.split()[1]
 
     def get_output(self, data, index):
         command = data[index]
@@ -104,22 +68,49 @@ class shell():
 
         return out
 
-    def get_file(self, s):
+    def get_dir_info(self, s):
+        _, name = s.split()
+        return name
+
+    def get_file_info(self, s):
         size, name = s.split()
         return int(size), name
 
     def is_dir(self, s):
-        return s.split()[0] == "dir"
-
-    def check_command(self, command, match):
-        return command.split()[1] == match
+        return re.match("^dir", s)
 
     def get_args(self, s):
-        return s.split()[-1]
+        return s.split()[2: ]
 
     def is_command(self, s):
-        return s[0] == "$"
+        return re.match("^\$", s)
+
+    # -------------------------------------------------------- Calculations ----------------------------------------------------------
+    def get_all_dir_sizes(self):
+        return [self.get_dir_size(key) for key in self.directories.keys()]
+
+    def get_dir_size(self, current_dir):
+        files_sizes = map(lambda x: x[0], self.directories[current_dir]["files"])
+        dir_size = sum(size for size in files_sizes)
+
+        for child in self.directories[current_dir]["dirs"]:
+            dir_size += self.get_dir_size(child)
+
+        return dir_size
 
 if __name__ == "__main__":
     shell = shell()
-    pretty_print(shell.part_1(), shell.part_2())
+
+    # Solution to part 1
+    sizes = shell.get_all_dir_sizes()
+    part1 = sum(s for s in sizes if s <= 100000)
+
+    # Solution to part 2
+    total_space = 70000000
+    required = 30000000
+    total_used = shell.get_dir_size("/")
+    not_used = total_space - total_used
+    to_be_freed = required - not_used
+    part2 = min(s for s in sizes if s >= to_be_freed)
+
+    pretty_print(part1, part2) 
